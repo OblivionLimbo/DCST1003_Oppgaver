ee68bc538046f1b3278400785e56487d72fc7e0a
BuildTools <unconfigured@null.spigotmc.org>
2021-11-11 16:17:59 +0100

    updated sql

--- practiceNine/src/mysql-pool.js
+++ practiceNine/src/mysql-pool.js
@@ -3,8 +3,8 @@ import mysql from 'mysql';
 // Setup MySQL-server connection pool
 export let pool = mysql.createPool({
   host: 'mysql-ait.stud.idi.ntnu.no',
-  user: 'username', // Replace "username" with your username
-  password: 'password', // Replace "password" with your password
-  database: 'username', // Replace "username" with your username
+  user: 'kristoju', // Replace "username" with your username
+  password: 'cLZm8ZII', // Replace "password" with your password
+  database: 'kristoju', // Replace "username" with your username
   connectionLimit: 1, // Reduce load on NTNU MySQL server
 });

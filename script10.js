9c0d4c32902ca4284bfd918bd8e41240f41d4765
BuildTools <unconfigured@null.spigotmc.org>
2021-11-16 13:11:08 +0100

    Ten

---
+++ script10.js
@@ -0,0 +1,12 @@
+let v = [1, 2, 3];
+
+// add 2 to each element of v: [ 3, 4, 5 ]
+
+let vPlus2 = v.map((number) => number + 2);
+console.log(vPlus2)
+
+// multiply each element of v by 2: [ 2, 4, 6 ]
+// sum of v: 6
+// mean of v: 2
+// each element of v as string: [ '1', '2', '3' ]
+// v as string: v[0] = 1, v[1] = 2, v[2] = 3

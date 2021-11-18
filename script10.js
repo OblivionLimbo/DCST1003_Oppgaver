8b731766f8134057a75a9e1531798ed4ddf57f5e
Oblivion <60346441+OblivionLimbo@users.noreply.github.com>
2021-11-18 12:41:34 +0100

    Mostly done

--- script10.js
+++ script10.js
@@ -4,36 +4,31 @@ let v = [1, 2, 3];
 // add 2 to each element of v: [ 3, 4, 5 ]
 
 console.log(
-  `Multiply each element of v by 2: 
-  ${v.map((a) => a + 2)}`
+  `Multiply each element of v by 2: ${v.map((a) => a + 2)}`
   );
 
 // multiply each element of v by 2: [ 2, 4, 6 ]
 
 console.log(
-  `Multiply each element of v by 2: 
-  ${v.map((a) => a * 2)}`
+  `Multiply each element of v by 2: ${v.map(a => a * 2)}`
   );
 
 // sum of v: 6
 
 console.log(
-  `Sum each element of v: 
-  ${v.reduce((a,b) => a + b)}`
+  `Sum each element of v: ${v.reduce((a,b) => a + b)}`
   );
 
 // mean of v: 2
 
 console.log(
-  `The mean of v: 
-  ${((v.reduce((a,b) => a + b)) / v.length)}`
+  `The mean of v: ${((v.reduce((a,b) => a + b)) / v.length)}`
   );
 
 // each element of v as string: [ '1', '2', '3' ]
 
 console.log(
-  `Each element of v as a string: 
-  ${JSON.stringify(v.map(String))}`
+  `Each element of v as a string: ${JSON.stringify(v.map(String))}`
   );
 
 // v as string: v[0] = 1, v[1] = 2, v[2] = 3
@@ -44,7 +39,8 @@ console.log(
   );
 
 function listArr(arr){
-
+  const a = arr.map((x,i) => ` v[${i}] = ${x}`);
+  return a;
 }
 
 // Task 2
@@ -55,27 +51,22 @@ let students = [
     { name: 'Knut', grade: 'C' },
   ];
 
+  
+
 // students-elements as strings: [ 'Ola got A', 'Kari got C', 'Knut got C' ]
 
 console.log(
-  `Students-elements as strings:
+  `Students-elements as strings: 
   ${listStud(students)}`
   );
 
 function listStud(arr){
-
-  const repeat = n => f => x =>
-    n === 0 ? Done(x) : Bounce(repeat(n-1) (f), f(x))
-  
-  let a = (`${students[0].name} got ${students[0].grade}`).toString();
-  let b = (`${students[1].name} got ${students[1].grade}`).toString();
-  let c = (`${students[2].name} got ${students[2].grade}`).toString();
-
-  let temp = [];
-  temp.push(a,b,c)
-  
-  return JSON.stringify(temp)
+  // let temp = [];
+  // arr.forEach(student => temp.push((`${student.name} got ${student.grade}`).toString()));
+  const a = (arr.map(e => `${e.name} got ${e.grade}`));
+  return JSON.stringify(a);
 }
+
 // Kari got: C
 
 console.log(
@@ -84,7 +75,7 @@ console.log(
   );
 
 function grade(student){
-  let a = students.find(({name}) => name === student);
+  const a = students.find(({name}) => name === student);
   return a.grade;
 }
 
@@ -92,33 +83,57 @@ function grade(student){
 
 console.log(
   `Did everyone get C:
-  ${"test"}`
+  ${everyoneC(students)}`
   );
+
+  function everyoneC(arr){
+    const a = arr.every(x => x.grade === 'C')
+    return a === true ? 'Yes' : 'No';
+  }
 
 // How many got C: 2
 
 console.log(
   `How many got C:
-  ${"test"}`
+  ${howManyC(students)}`
   );
+
+  function howManyC(arr){
+    const a = arr.filter(x => x.grade === 'C');
+    return a.length;
+  }
 
 // Percentage of C grades: 66.66666666666667
 
 console.log(
   `Percentage of C grades:
-  ${"test"}`
+  ${percentageC(students)}`
   );
+
+  function percentageC(arr){
+    return howManyC(arr) / arr.length * 100;
+  }
 
 // Did anyone get A: Yes
 
 console.log(
   `Did anyone get A:
-  ${"test"}`
+  ${anyoneA(students)}`
   );
+
+  function anyoneA(arr){
+    const a = arr.some(x => x.grade === 'A' )
+    return a === true ? 'Yes' : 'No';
+  }
 
 // Did anyone get F: No
 
 console.log(
   `Did anyone get F:
-  ${"test"}`
+  ${anyoneF(students)}`
   );
+
+  function anyoneF(arr){
+    const a = arr.some(x => x.grade === 'F' )
+    return a === true ? 'Yes' : 'No';
+  }
